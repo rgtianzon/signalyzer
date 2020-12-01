cb93d234ab6e167d4b217ac5d88cac2792eddbeb
Rupert Tianzon <rupert.tianzon@EXTENDBPO.LOCAL>
2020-12-01 09:21:19 -0800

    make it better

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.engine('ejs', ejsMate)
+// app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.get('/', (req, res) => {
+    res.render('home');
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
